7c35fbba855db32e547464d5e6e733f14e0f70e5
smikes <smikes@cubane.com>
2014-05-06 07:52:34 -0600

    initial rev

---
+++ ch01/Spec.hs
@@ -0,0 +1,10 @@
+-- file Spec.hs
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+
+main :: IO ()
+main = hspec $ do
+  describe "Prelude.head" $ do
+    it "returns the first element of a list" $ do
+      head [23 ..] `shouldBe` (23 :: Int)
